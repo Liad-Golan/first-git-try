544f8478242756332bb17234601cf91abdacabac
Liad-Golan <liad.s.golan@gmail.com>
2023-02-19 18:07:00 +0200

    ~

--- First_git_try.py
+++ First_git_try.py
@@ -3,3 +3,5 @@ print("Hello, this is our first try with Github\nWe are very excited amigos!")
 print("we're also have a test tomorrow so see ya :)")
 
 print("Let's play 'Continue The Song', for example the song: la la la la la")
+
+print('i dare you.... LEGO. \nnow try to guess who sing this song')
