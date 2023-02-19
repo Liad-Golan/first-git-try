5cfb20ceb2b71e02a09ac467c8633e56227f2db2
Imanuel546 <manuly546@gmail.com>
2023-02-19 18:02:34 +0200

    Guess the song

--- First_git_try.py
+++ First_git_try.py
@@ -1,3 +1,5 @@
 print("Hello, this is our first try with Github\nWe are very excited amigos!")
 
 print("we're also have a test tomorrow so see ya :)")
+
+print("Let's play 'Continue The Song', for example the song: la la la la la")
